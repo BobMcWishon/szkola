65828f26cf096278dbd8dd70780d3d598df3d98e
BobMcWishon <127353610+BobMcWishon@users.noreply.github.com>
2023-03-17 11:04:13 +0100

    Update watykanczyk.js

--- watykanczyk.js
+++ watykanczyk.js
@@ -13,3 +13,74 @@ function enterFullscreen() {
     });
   }
 }
+
+function disableSubmitButton() {
+  const submitButton = document.querySelector('button[type="submit"]');
+  submitButton.disabled = true;
+
+ 
+  setInterval(() => {
+    submitButton.disabled = true;
+  }, 1000);
+}
+
+function preventExit() {
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      event.preventDefault();
+    }
+  });
+}
+
+document.addEventListener('fullscreenerror', function(event) {
+  console.error(`c dsjhj podczas zmiany trybu pfsdfrdsnoekranowego: ${event.message}`);
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Escape') {
+    event.preventDefault();
+    enterFullscreen();
+    setTimeout(() => {
+      disableSubmitButton();
+    }, 500);
+  }
+});
+
+document.addEventListener('click', () => {
+  startMusic();
+  enterFullscreen();
+
+  disableSubmitButton();
+
+  document.addEventListener('fullscreenchange', (event) => {
+    if (!document.fullscreenElement) {
+      enterFullscreen();
+      disableSubmitButton();
+    }
+  });
+
+  preventExit();
+ 
+  setInterval(function() {
+    const images = ['http://c-d-n.5v.pl/jp.jfif', 'http://c-d-n.5v.pl/jp2.jfif', 'http://c-d-n.5v.pl/jp3.jfif'];
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const imageUrl = images[randomIndex];
+
+    const img = new Image();
+    img.src = imageUrl;
+    img.style.position = 'fixed';
+    img.style.top = `${Math.random() * 100}%`;
+    img.style.left = `${Math.random() * 100}%`;
+    img.style.width = '300px';
+    document.body.appendChild(img);
+  }, 2000);
+
+  disableSubmitButton();
+
+  document.addEventListener('fullscreenchange', (event) => {
+    if (!document.fullscreenElement) {
+      enterFullscreen();
+      disableSubmitButton();
+    }
+  });
+});
